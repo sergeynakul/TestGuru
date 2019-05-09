cfd942a81e6dddaac7a15359ee3c29159c9b2b53
Sergio <nakul.sv@gmail.com>
2019-05-09 14:15:54 +0300

    Add static pages about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'about', to: 'about#index'
+  get 'about/author'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
