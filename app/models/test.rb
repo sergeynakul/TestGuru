b0c177a4ea00e9a21d3cef3ead98154e93313cfc
Sergio <nakul.sv@gmail.com>
2019-05-22 20:57:29 +0300

    fix bags

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,11 +3,11 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :passed_tests
   has_many :users, through: :passed_tests
-  
+
   validates :title, presence: true
   validates :level, numericality: { only_integer: true }
-  validates :title, uniqueness: { scope: level }
-  
+  validates :title, uniqueness: { scope: :level }
+
   scope :simple_tests, -> { where level: 0..1 }
   scope :middle_tests, -> { where level: 2..4 }
   scope :difficult_tests, -> { where level: 5..Float::INFINITY }
