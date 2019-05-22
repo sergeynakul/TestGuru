cf6e293db227754416d6662df9f75fc6bbacb8b0
Sergey <nakul.sv@gmail.com>
2019-05-22 11:43:25 +0000

    add scopes and validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  
+  validates :body, presence: true
+  validate :answer_count
+  
+  scope :correct_answers, -> { where correct: true }
+  
+  def answer_count
+    errors.add(:base, "У одного вопроса может быть от 1 до 4 ответов") if Answer.where(question_id: self.question_id).count != 1..4
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :answers
   has_many :passed_tests
   has_many :tests, through: :passed_tests
-
+  
+  validates :name, presence: true
+  
   def passed_tests_by(level)
     Test.joins(:passed_tests).where(passed_tests: { user_id: id }, level: level).pluck(:title)
   end
