40fe5ae904f204481f7dc81617a0a5f9800a2960
Sergio <nakul.sv@gmail.com>
2019-06-01 16:51:41 +0300

    refactor result message

--- app/helpers/passed_tests_helper.rb
+++ app/helpers/passed_tests_helper.rb
@@ -1,5 +1,12 @@
 module PassedTestsHelper
-  def percentage_success
-    @passed_test.correct_questions * 100 / @passed_test.test.questions.count
+  SUCCESS_RATE = 85
+
+  def result_message
+    percentage_success = @passed_test.correct_questions * 100 / @passed_test.test.questions.count
+    if percentage_success >= SUCCESS_RATE
+      content_tag(:p, "You have successfully passed the test, the percentage of success - #{percentage_success}%", style: 'color: green')
+    else
+      content_tag(:p, "Test failed, the percentage of success - #{percentage_success}%", style: 'color: red')
+    end
   end
 end
