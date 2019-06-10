9bf7e0fc58b9aca85ac1b31c9c3334a1b481f0e5
Sergio <nakul.sv@gmail.com>
2019-06-10 21:53:32 +0300

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(name: 'Sergey', type: 'Admin', email: 'example@mail.com', confirmed_at: '2019-06-10 18:38:16.764429', password: '123456',
+            password_confirmation: '123456')
 Category.create(title: 'Ruby')
 Test.create(title: 'Ruby basics', category_id: 1, admin_id: 1)
 Question.create(body: 'Who created the ruby language?', test_id: 1)
